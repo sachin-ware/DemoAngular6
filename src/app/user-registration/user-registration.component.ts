b4906d1ab9027a52ba363fb5c5688b38ae2ea6ee
sachin-ware <sachinware813@gmail.com>
2018-09-03 02:00:37 +0530

    avatar added in user list

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -13,11 +13,11 @@ export class UserRegistrationComponent implements OnInit {
   selectedFile: File; 
 
   user:IUser={
-    fname: 'sachin',
-    lname: 'ware',
-    email: 'sachin.ware@cognizant.com',
+    fname: '',
+    lname: '',
+    email: '',
     dob: '2018-05-01',//backend needs YYYY-MM-DD format to save user.
-    userName: 'sachin',
+    userName: '',
     password: 'password',
     proPicUrl:"https://TestURL"
   }
@@ -40,8 +40,8 @@ export class UserRegistrationComponent implements OnInit {
  uploadToCloud(){
    const uploadData = new FormData();
    uploadData.append('file', this.selectedFile,);
-   this.http.post('http://localhost:8082/uploadProfilePic/'+this.user.userId, uploadData,{responseType: 'text'} ).subscribe(event => {
-    //this.http.post('https://sachin-ware-sb-rest-server.herokuapp.com/uploadPic', uploadData,{responseType: 'text'} ).subscribe(event => {
+   //this.http.post('http://localhost:8082/uploadProfilePic/'+this.user.userId, uploadData,{responseType: 'text'} ).subscribe(event => {
+    this.http.post('https://sachin-ware-sb-rest-server.herokuapp.com/uploadProfilePic/'+this.user.userId, uploadData,{responseType: 'text'} ).subscribe(event => {
     
        console.log('Event:',event); // handle event here
        this.avatarUrl=event;//.replace('File uploaded successfully:','');
@@ -51,12 +51,13 @@ export class UserRegistrationComponent implements OnInit {
  saveClicked(){
   console.log('saving User:',JSON.stringify(this.user));
 
- // this.http.post('https://sachin-ware-sb-rest-server.herokuapp.com/user', this.user).subscribe(res=>{
-    this.http.post('http://localhost:8082/user', this.user).subscribe(res=>{
-
+  this.http.post('https://sachin-ware-sb-rest-server.herokuapp.com/user', this.user).subscribe(res=>{
+ //   this.http.post('http://localhost:8082/user', this.user).subscribe(res=>{
+      
       console.log('Saved User:',JSON.stringify(res));
    // {"userId":33,"userName":"sachin","password":"password","email":"sachin.ware@cognizant.com","fname":"sachin","lname":"ware","dob":1525132800000,"createdDate":1535865316208,"modifiedDate":1535865316208,"lastAccessed":null,"proPicUrl":"https://TestURL"}
-     // this.user=res;
+    let tmpuser : any =res;
+    this.user=tmpuser;
 
   });
   
